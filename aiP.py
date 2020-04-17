327a050630f2db3abc811fc4064aea6b95749df2
Thomas Ferrer <thomas.ferrer@hei.yncrea.fr>
2020-04-17 17:03:29 +0200

    keras

--- aiP.py
+++ aiP.py
@@ -9,7 +9,15 @@ import pandas as pd
 import numpy as np
 import os 
 import shutil
+
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import image
+
 
 #
 # Part 1 : preprocessing
@@ -52,6 +60,72 @@ def img_to_folder(name_folder, img_list, attractive_list):
         
 img_to_folder('./Trainning',x_train,y_train)
 img_to_folder('./Testing',x_test,y_test)
+
+
+#
+# Part 2 : building model
+#
+
+# init CNN 
+classifier = Sequential()
+classifier.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(64,64,3)))
+classifier.add(MaxPool2D(pool_size=(2,2)))
+classifier.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+classifier.add(MaxPool2D(pool_size=(2,2)))
+classifier.add(Flatten())
+
+# init ANN
+classifier.add(Dense(units=128,activation='relu'))
+classifier.add(Dense(units=1,activation='sigmoid'))
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+#
+# Part 3 : fitting images to CNN
+#
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+        )
+
+training_set = train_datagen.flow_from_directory('./Trainning',
+                                                 target_size=(64,64), 
+                                                 batch_size=32, 
+                                                 class_mode='binary')
+
+hist = classifier.fit_generator(
+        training_set, 
+        steps_per_epoch=(len(x_train)/32),
+        epochs=5
+        )
+
+# save the model
+classifier.save('attrative_cnn.h5')
+
+#
+# Part 4 : Prediction 
+#
+
+# Predict image
+#test_image = image.load_img('./image_test/img3.jpg', target_size=(64,64))
+#test_image = image.img_to_array(test_image).astype('float32')/255
+#test_image = np.expand_dims(test_image, axis=0)
+
+#result = classifier.predict(test_image)
+
+# Predict images
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = train_datagen.flow_from_directory('./Testing',
+                                                 target_size=(64,64), 
+                                                 batch_size=32, 
+                                                 class_mode='binary')
+y_predict = classifier.predict_generator(test_set,steps=len(test_set))
+
+# Confusion Matrix
+y_predict = y_predict > 0.5
+cm = confusion_matrix(y_test, y_predict)
 
 
 
